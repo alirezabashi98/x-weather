86f24ed6d6c9913cd86beb0a2416d3e130235e0e
Alireza Bashi <alirezabashi98@gmail.com>
2024-01-02 08:29:00 +0330

    add dioProvider + add package darz for either

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,12 +1,10 @@
-import 'package:awesome_dio_interceptor/awesome_dio_interceptor.dart';
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 
+import 'utils/api_provider.dart';
 
 final locator = GetIt.instance;
 
-Future<void> initializeDependencies() async{
-  
-
-  locator.registerSingleton<Dio>(dio);
+Future<void> initializeDependencies() async {
+  locator.registerSingleton<Dio>(DioProvider.createDio());
 }
