e8a3cf1659accea543346628a250d17411e641e6
علیرضا باشی <alirezabashi98@gmail.com>
2024-01-03 18:49:18 +0330

    add LinearGradient

--- lib/utils/constants/constants.dart
+++ lib/utils/constants/constants.dart
@@ -1,6 +1,34 @@
+import 'dart:ui';
+
 class Constants {
   static String baseUrl = 'https://api.openweathermap.org';
   static String defaultApiKey = 'b7a0e0de64b16d1326ef62d97bebcd61';
 
   static String errorMessage = 'An error occurred... Please try again';
+
+  static Color primary = const Color(0x00ffffff);
+  static Color secondary = const Color(0x00ebebf5);
+  static Color tertiary = const Color(0x00ebebf5);
+  static Color quaternary = const Color(0x00ebebf5);
+
+  static List<Color> linera1 = [
+    const Color(0x002e335a),
+    const Color(0x001c1b33)
+  ];
+  static List<Color> linera2 = [
+    const Color(0x005936b4),
+    const Color(0x00362a84)
+  ];
+  static List<Color> linera3 = [
+    const Color(0x003658b1),
+    const Color(0x00c159ec)
+  ];
+  static List<Color> linera4 = [
+    const Color(0x00aec9ff),
+    const Color(0x00083072)
+  ];
+  static List<Color> linera5 = [
+    const Color(0x00f7cbfd),
+    const Color(0x007758d1)
+  ];
 }
